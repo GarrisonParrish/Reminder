a81afd394ddc5171aa94597b16f9d379d8b78988
Garrison Parrish <tpgarris@live.unc.edu>
2021-12-22 11:26:46 -0500

    demo

--- main/notif.py
+++ main/notif.py
@@ -3,6 +3,7 @@
 import time
 import pync  # allows program to send user notifications to Mac OS operating system
 from reminder import Reminder
+# from read_write import read_to_reminder
 
 
 def notify(r: Reminder) -> None:
@@ -14,3 +15,7 @@ def notify_delay(r: Reminder, interval: int) -> None:
     time.sleep(interval)
     # get title from json file
     pync.notify(r.getNotes(), title="Reminder", subtitle=r.getTitle())
+
+
+
+# notify(read_to_reminder("data/Get a unicorn.json"))
