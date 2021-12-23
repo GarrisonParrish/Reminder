692f5628ba174d5e2d837cdc3bed20d3d589f6b8
Garrison Parrish <tpgarris@live.unc.edu>
2021-12-23 12:58:27 -0500

    home page with image

--- gui/main_app.py
+++ gui/main_app.py
@@ -1,7 +1,10 @@
 """Root application window."""
 
 import tkinter as tk
+from tkinter.constants import NW
 from gui.input_reminder import InputReminder
+from main.constants import APP_TITLE, FEELS_GOOD_PATH
+from PIL import Image, ImageTk
 
 # Run with 'python -m gui.root'
 
@@ -9,10 +12,35 @@ class MainApp(tk.Frame):
     """Inherits from tk.Frame"""
     def __init__(self, master):
         tk.Frame.__init__(self, master) # self is a frame, parent is master
+        self.label = tk.Label(
+            self,
+            text=APP_TITLE,
+            font=("Helvetica", 40)
+        )
+        self.label.pack(padx=60, pady=40)
+
+        self.canvas = tk.Canvas(self, width= 400, height= 300)
+        self.canvas.pack()
+
+        self.image = Image.open(FEELS_GOOD_PATH)
+        self.resized_image = self.image.resize((400, 293), Image.ANTIALIAS)
+        self.new_image = ImageTk.PhotoImage(self.resized_image)
+
+        self.canvas.create_image(10, 10, anchor=NW, image=self.new_image)
         
+        self.open_button = tk.Button(self, text="New", command=self.open)
+        self.open_button.pack()
+        self.exit_button = tk.Button(self, text="Exit", command=self.exit)
+        self.exit_button.pack()
     
-    def input_reminder(self):
-        self.child_window = InputReminder()
+    def open(self):
+        """Open the application (go to home page window)"""
+        print("Opened!")
+        # TODO: implement (destroy current frame, build the home page frame inside the root window)
+    
+    def  exit(self):
+        self.master.destroy()
+    
 
 
 if __name__ == "__main__":
---
+++ main/constants.py
@@ -0,0 +1,2 @@
+APP_TITLE = "Reminder App"
+FEELS_GOOD_PATH = "./main/assets/feelsgoodman.png"
